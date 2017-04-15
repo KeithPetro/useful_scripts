fa64950655852f3608264696e7ac3333b22903fb
Keith Petro <keith.petro@usask.ca>
2017-04-15 15:50:04 -0600

    Update standard_functions.py

--- CME_451/standard_functions.py
+++ CME_451/standard_functions.py
@@ -5,6 +5,9 @@
 #Name: standard_functions
 #Purpose: holds important functions
 
+#Dependencies (must be in same directory as this file):
+#   N/A
+
 #Convert from hexadecimal string to binary string
 def convertHexToBin(hexString):
     hexVals = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6': '0110', '7': '0111', '8': '1000', '9': '1001', 'a': '1010', 'b': '1011', 'c': '1100', 'd': '1101', 'e': '1110', 'f': '1111'}
